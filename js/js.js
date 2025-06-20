944cd5631b15ba312c6e083ab1d9de5b2951a0aa
vlaad53 <163895253+vlaad53@users.noreply.github.com>
2025-06-20 18:56:07 +0300

    32321

--- js/js.js
+++ js/js.js
@@ -19,6 +19,28 @@ input.addEventListener("input", () => {
   }
 });
   
+// Напиши скрипт, який би при втраті фокуса на інпут, перевіряв його вміст на правильну кількість символів.
+// Скільки символів має бути в інпут, вказується в його атрибуті data-length.
+// Якщо введена відповідна кількість, то border інпут стає зеленим,   якщо неправильне - червоним.
+{/* <input
+  type="text"
+  id="validation-input"
+  data-length="6"
+  placeholder="Введи 6 символів"
+/>; */}
+const inpEl = document.querySelector("#validation-input");
+inpEl.addEventListener("blur", (event) => {
+  const dataLen = Number(parseInt(inpEl.dataset.length));  
+  if (event.currentTarget.value.length === dataLen) {
+    inpEl.style.border = "3px solid green"
+  } else {
+     inpEl.style.border = "3px solid red";
+  }
+
+  console.log(dataLen);
+})
+
+// const dataLenght = parseInt(inpEl.dataset.lenght)
 
 // 3. Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text оновлюючи властивість font-size. В результаті при перетягуванні повзунка змінюватиметься розмір тексту.
 
